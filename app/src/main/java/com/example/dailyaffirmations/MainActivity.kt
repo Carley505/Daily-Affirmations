e59bae4179b0168a371e9775aab584d366db6d28
Carley505 <callistusngeywa@gmail.com>
2026-05-05 15:30:59 +0300

    re-arrange screen components and resize afresh

--- app/src/main/java/com/example/dailyaffirmations/MainActivity.kt
+++ app/src/main/java/com/example/dailyaffirmations/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.example.dailyaffirmations
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.dailyaffirmations.data.DataSource
+import com.example.dailyaffirmations.model.Affirmation
 import com.example.dailyaffirmations.ui.theme.DailyAffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +40,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DailyAffirmationsTheme {
+                val layoutDirection = LocalLayoutDirection.current
                 Surface(
-                    modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())
+                    modifier = Modifier.fillMaxSize().statusBarsPadding()
+                        .padding(
+                            start = WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateStartPadding(layoutDirection),
+                            end = WindowInsets.safeDrawing.asPaddingValues()
+                                .calculateEndPadding(layoutDirection)
+                        )
                 ) {
                     DailyAffirmationApp()
                 }
@@ -42,77 +57,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Affirmation(
-    val image: Int,
-    val affirmationText: Int
-)
-
-val affirmations = listOf(
-    Affirmation(
-        image = R.drawable.image1,
-        affirmationText = R.string.txt_1
-    ),
-    Affirmation(
-        image = R.drawable.image2,
-        affirmationText = R.string.txt_2
-    ),
-    Affirmation(
-        image = R.drawable.image3,
-        affirmationText = R.string.txt_3
-    ),
-    Affirmation(
-        image = R.drawable.image4,
-        affirmationText = R.string.txt_4
-    ),
-    Affirmation(
-        image = R.drawable.image5,
-        affirmationText = R.string.txt_5
-    ),
-    Affirmation(
-        image = R.drawable.image6,
-        affirmationText = R.string.txt_6
-    ),
-    Affirmation(
-        image = R.drawable.image7,
-        affirmationText = R.string.txt_7
-    ),
-    Affirmation(
-        image = R.drawable.image8,
-        affirmationText = R.string.txt_8
-    ),
-    Affirmation(
-        image = R.drawable.image9,
-        affirmationText = R.string.txt_9
-    ),
-    Affirmation(
-        image = R.drawable.image10,
-        affirmationText = R.string.txt_10
-    ),
-)
-
 @Composable
 fun DailyAffirmationApp() {
-    Column() {
-        affirmations.forEach {
-            AffirmationElement(it)
+    AffirmationList(DataSource().loadAffirmations())
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationElement(
+                item = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
 
 @Composable
-fun AffirmationElement(item: Affirmation) {
-    Column(
-        modifier = Modifier.padding(16.dp).fillMaxWidth().background(Color.DarkGray)
-    ) {
-        Image(
-            painter = painterResource(item.image),
-            contentDescription = "image_1",
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            text = stringResource(item.affirmationText),
-            fontSize = 8.sp
-        )
+fun AffirmationElement(item: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(item.imageResourceId),
+                contentDescription = stringResource(item.stringResourceId),
+                modifier = Modifier.fillMaxWidth().height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(item.stringResourceId),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
 
--- app/src/main/java/com/example/dailyaffirmations/data/DataSource.kt
+++ app/src/main/java/com/example/dailyaffirmations/data/DataSource.kt
@@ -1,4 +1,51 @@
 package com.example.dailyaffirmations.data
 
-class DataSource {
+import com.example.dailyaffirmations.R
+import com.example.dailyaffirmations.model.Affirmation
+
+class DataSource() {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf(
+            Affirmation(
+                imageResourceId = R.drawable.image1,
+                stringResourceId = R.string.txt_1
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image2,
+                stringResourceId = R.string.txt_2
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image3,
+                stringResourceId = R.string.txt_3
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image4,
+                stringResourceId = R.string.txt_4
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image5,
+                stringResourceId = R.string.txt_5
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image6,
+                stringResourceId = R.string.txt_6
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image7,
+                stringResourceId = R.string.txt_7
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image8,
+                stringResourceId = R.string.txt_8
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image9,
+                stringResourceId = R.string.txt_9
+            ),
+            Affirmation(
+                imageResourceId = R.drawable.image10,
+                stringResourceId = R.string.txt_10
+            )
+        )
+    }
 }
